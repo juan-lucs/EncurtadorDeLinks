516710664abb0d86afcdec36262c662cae9135ce
juan-lucs <ls2699120@gmail.com>
2026-09-18 17:49:24 -0300

    chore: cria projeto

---
+++ src/main/java/com/juanlucas/encurtador_de_links_api/EncurtadorDeLinksApiApplication.java
@@ -0,0 +1,13 @@
+package com.juanlucas.encurtador_de_links_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncurtadorDeLinksApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncurtadorDeLinksApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juanlucas/encurtador_de_links_api/EncurtadorDeLinksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanlucas.encurtador_de_links_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncurtadorDeLinksApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

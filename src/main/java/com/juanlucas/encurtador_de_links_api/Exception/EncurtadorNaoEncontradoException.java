1d3be18d84d74237e6816f01044b56cdca4a604d
juan-lucs <ls2699120@gmail.com>
2026-09-20 02:38:28 -0300

    feat: adiciona exceções para URL vazia e link não encontrado

---
+++ src/main/java/com/juanlucas/encurtador_de_links_api/Exception/EncurtadorNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package com.juanlucas.encurtador_de_links_api.Exception;
+
+public class EncurtadorNaoEncontradoException extends Exception{
+    public EncurtadorNaoEncontradoException(String s) {
+        super(s);
+    }
+}
---
+++ src/main/java/com/juanlucas/encurtador_de_links_api/Exception/UrlVaziaException.java
@@ -0,0 +1,5 @@
+package com.juanlucas.encurtador_de_links_api.Exception;
+
+public class UrlVaziaException extends Exception {
+    public UrlVaziaException(String m) {super(m);}
+}

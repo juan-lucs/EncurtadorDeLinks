f01ccb19eb846d91e2c6238925d0397dbf6c43c4
juan-lucs <ls2699120@gmail.com>
2026-09-20 12:58:13 -0300

    Valida formato da URL em CreateLinkRequest

--- src/main/java/com/juanlucas/encurtador_de_links_api/Model/DTO/CreateLinkRequest.java
+++ src/main/java/com/juanlucas/encurtador_de_links_api/Model/DTO/CreateLinkRequest.java
@@ -1,7 +1,10 @@
 package com.juanlucas.encurtador_de_links_api.Model.DTO;
 
+import jakarta.validation.constraints.NotBlank;
 
 public class CreateLinkRequest {
+
+    @NotBlank
     private String url;
     public CreateLinkRequest() {
     }

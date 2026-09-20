38746c99509e5a0e836e1ac9dc5f8f24fe715afb
juan-lucs <ls2699120@gmail.com>
2026-09-20 18:54:24 -0300

    feat: valida URL, gera código único e registra estatísticas no LinkService

--- src/main/java/com/juanlucas/encurtador_de_links_api/Service/LinkService.java
+++ src/main/java/com/juanlucas/encurtador_de_links_api/Service/LinkService.java
@@ -1,57 +1,79 @@
 package com.juanlucas.encurtador_de_links_api.Service;
 
 import com.juanlucas.encurtador_de_links_api.Exception.EncurtadorNaoEncontradoException;
-import com.juanlucas.encurtador_de_links_api.Exception.UrlVaziaException;
-import com.juanlucas.encurtador_de_links_api.Model.DTO.CreateLinkRequest;
+import com.juanlucas.encurtador_de_links_api.Exception.UrlInvalidaException;
+import com.juanlucas.encurtador_de_links_api.Model.DTO.CriarLinkRequest;
+import com.juanlucas.encurtador_de_links_api.Model.DTO.SaidaLinkRequest;
+import com.juanlucas.encurtador_de_links_api.Model.Entity.EstatisticaLink;
 import com.juanlucas.encurtador_de_links_api.Model.Entity.Link;
+import com.juanlucas.encurtador_de_links_api.Repository.estatisticaLinkRepository;
 import com.juanlucas.encurtador_de_links_api.Repository.linkRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.view.RedirectView;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.time.LocalDate;
-import java.util.Random;
-import java.util.UUID;
+import java.time.LocalDateTime;
+import java.util.*;
 
 @Service
 public class LinkService {
-    private final linkRepository Repository;
+    private final linkRepository Repositorylink;
+    private final estatisticaLinkRepository Repositoryesta;
 
-    public LinkService(linkRepository repository) {
-
-        Repository = repository;
+    public LinkService(linkRepository repository, estatisticaLinkRepository repositoryesta) {
+        Repositoryesta = repositoryesta;
+        Repositorylink = repository;
     }
 
-    private static String gerarCodigo() {
+    private String gerarCodigo() {
         var caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
         var codigo = new StringBuilder("");
         var random = new Random();
         for (var i = 0; i <5; i++) {
             codigo.append(caracteres.charAt(random.nextInt(caracteres.length())));
-
+        }
+        List<String> codigos = Repositorylink.findAllCodigos();
+        if (codigos.contains(codigo.toString())) {
+            return gerarCodigo();
         }
         return codigo.toString();
     }
+    private static boolean validaUrl(String url) {
+        try {
+            var uri = new URI(url); // analisa a estrutura da url
+            if ("http".equalsIgnoreCase(uri.getScheme()) || "https".equalsIgnoreCase(uri.getScheme()) && uri.getHost() != null) {
+                return true;
+            } else { return false;}
+        } catch (URISyntaxException e) {
+            return false;
+        }
+    }
 
-    public Link encurtarLink(CreateLinkRequest url) throws UrlVaziaException {
-        // ADICIONAR VERIFICADOR DE URL (SE ELA EXISTE E NAO DA 404)
-        var link = new Link(UUID.randomUUID().toString(), url.getUrl(), gerarCodigo(), LocalDate.now());
-        Repository.save(link);
-        return link;
+    public Link encurtarLink(CriarLinkRequest url) throws UrlInvalidaException {
+        if (!validaUrl(url.getUrl())) {
+            throw new UrlInvalidaException("Url inválida!");
+        }
+        var link = new Link(UUID.randomUUID().toString(), url.getUrl(), gerarCodigo());
+        var estatisticas = new EstatisticaLink(0 , LocalDate.now(), new LinkedList<LocalDateTime>());
+        link.setEstatisticaLink(estatisticas);
+        return Repositorylink.save(link);
     }
 
     public Link buscarLink(String codigo) throws EncurtadorNaoEncontradoException{
-            var link = Repository.findByCodigo(codigo);
-            if (link == null) {
+        var link = Repositorylink.findByCodigo(codigo);
+        if (link == null) {
                 throw new EncurtadorNaoEncontradoException("Url encurtada não encontrada!");
-            }
-            link.setCliques(link.getCliques() + 1);
-            Repository.save(link);
-            return link;
+        }
+        EstatisticaLink estatistica = Repositoryesta.findByLinkId(link.getId());
+        estatistica.setCliques(estatistica.getCliques() + 1);// Tentar UPDATE ATÔMICO BOOM
+        estatistica.getUltimoAcessos().add(LocalDateTime.now());
+        link.setEstatisticaLink(estatistica);
+        return Repositorylink.save(link);
         }
 
     public Link buscarcliques(String codigo) {
-        var link = Repository.findByCodigo(codigo);
+        var link = Repositorylink.findByCodigo(codigo);
         return link;
     }
 }

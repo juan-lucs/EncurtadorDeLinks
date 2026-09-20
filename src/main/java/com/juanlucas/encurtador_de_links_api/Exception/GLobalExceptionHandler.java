8ee3c9fe8e7c3cc78c9e1bf40d1bf88a47687090
juan-lucs <ls2699120@gmail.com>
2026-09-20 18:54:24 -0300

    feat: adiciona UrlInvalidaException e tratamento global de exceções

---
+++ src/main/java/com/juanlucas/encurtador_de_links_api/Exception/GLobalExceptionHandler.java
@@ -0,0 +1,42 @@
+package com.juanlucas.encurtador_de_links_api.Exception;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+@RestControllerAdvice
+public class GLobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ProblemDetail UrlVazia(MethodArgumentNotValidException m ) {
+        String mensagemTratada = m.getBindingResult()
+                .getFieldErrors()
+                .get(0)
+                .getDefaultMessage();
+        var problemDetail= ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, mensagemTratada);
+        problemDetail.setTitle("Url inválida!");
+        return problemDetail;
+    }
+
+    @ExceptionHandler(EncurtadorNaoEncontradoException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ProblemDetail UrlNaoEncontrada(EncurtadorNaoEncontradoException m) {
+        var problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, m.getMessage());
+        problemDetail.setTitle("Link encurtado não encontrado");
+        return problemDetail;
+    }
+
+    @ExceptionHandler(UrlInvalidaException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ProblemDetail UrlInvalida(UrlInvalidaException m) {
+        var problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, m.getMessage());
+        problemDetail.setTitle("Formato da URL não aceito ou inválido!");
+        return problemDetail;
+    }
+}
---
+++ src/main/java/com/juanlucas/encurtador_de_links_api/Exception/UrlInvalidaException.java
@@ -0,0 +1,7 @@
+package com.juanlucas.encurtador_de_links_api.Exception;
+
+public class UrlInvalidaException extends Exception {
+    public UrlInvalidaException(String s) {
+        super(s);
+    }
+}
